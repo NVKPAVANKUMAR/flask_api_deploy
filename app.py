38ffda43cb3d6750136fc1d2d84f4124fe20c57b
NVKPAVANKUMAR <npavankumar144@gmail.com>
2019-08-05 22:28:27 +0530

    Get Request added

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
+stores = [
+    {
+        "name": "Dmart",
+        "items": [
+            {
+                "name": "Snikers",
+                "price": 25
+            }
+        ]
+    }
+]
 
 @app.route('/')
 def index():
     return 'OK!'
+
+@app.route('/store')
+def get_stores():
+    return jsonify({"stores": stores})
 
 if __name__ == '__main__':
     app.run()
